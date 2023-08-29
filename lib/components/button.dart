8442984161c18de0d95ba51bdb630ee9fdbeaf95
Villy15 <adrianvill07@gmail.com>
2023-08-29 22:57:45 +0800

    Add login register pages

---
+++ lib/components/button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  final Function()? onTap;
+  final String text;
+
+  const MyButton({super.key, this.onTap, required this.text});
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      decoration: const BoxDecoration(
+        // Color hex 667080
+        color: Color(0xff667080),
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
+      child: Center(
+        child: Text(
+          widget.text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          )
+        ),
+      )
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,148 @@
+import 'package:cooptourism/components/button.dart';
+import 'package:cooptourism/components/text_field.dart';
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+
+  const LoginPage({super.key, this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [          
+                  
+                  
+            
+                  // New here? register now
+                   Row(
+                    children: [
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Inter",
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
+                  ),
+            
+                  Row(
+                    children: [
+                       Text(
+                        "New here? ",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: Text(
+                          "Register now!",
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+            
+                  const SizedBox(height: 75),
+            
+                  
+                  // email textfield
+                  MyTextField(
+                        controller: emailTextController,
+                        hintText: "Email",
+                        obscureText: false
+                      ),
+            
+                  const SizedBox(height: 10),
+                    
+                  
+                  // password textfield
+                  MyTextField(
+                        controller: passwordTextController,
+                        hintText: "Password",
+                        obscureText: true
+                      ),
+            
+                  const SizedBox(height: 10),
+            
+                  
+                  // login button 
+                  MyButton(
+                    onTap: () {},
+                    text: "Login",
+                  ),
+            
+                  const SizedBox(height: 20),
+                  
+                  // Forgot password
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 50),
+            
+                  // go to register page
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                       Text(
+                        "To register as a cooperative, ",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      GestureDetector(
+                        // onTap: widget.onTap,
+                        child: Text(
+                          "click here",
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
