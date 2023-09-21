7b9f3718938042af688d3f4ff2cbf7c176c05d06
Mihoytimoy <timothy_mendoza@dlsu.edu.ph>
2023-09-21 22:09:36 +0800

    commented drawer and added bottom nav

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,52 +26,68 @@ class _HomePageState extends State<HomePage> {
           iconTheme:
               IconThemeData(color: Theme.of(context).secondaryHeaderColor),
         ),
-        drawer: Container(
-          width: 250,
-          child: Drawer(
-              backgroundColor: Theme.of(context).primaryColor,
-              child: Column(
-                children: <Widget>[
-                  ListView(shrinkWrap: true, children: [
-                    DrawerHeader(
-                        child: Center(
-                      child: Text("Turistanginamo",
-                          style: Theme.of(context).textTheme.headlineMedium),
-                    )),
-                    ListTile(
-                      leading: const Icon(Icons.home),
-                      iconColor: Theme.of(context).secondaryHeaderColor,
-                      title: Text(
-                        "Home",
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.groups_outlined),
-                                            iconColor: Theme.of(context).secondaryHeaderColor,
-                      title: Text(
-                        "Cooperatives",
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      onTap: () {},
-                    ),
-                  ]),
-                  Expanded(
-                      child: Align(
-                    alignment: FractionalOffset.bottomLeft,
-                    child: ListTile(
-                      leading: const Icon(Icons.logout),
-                                            iconColor: Theme.of(context).secondaryHeaderColor,
-                      title: Text(
-                        "Logout",
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      onTap: signOut,
-                    ),
-                  ))
-                ],
-              )),
-        ));
+        bottomNavigationBar: GNav(
+          backgroundColor: Theme.of(context).primaryColor,
+          color: Theme.of(context).secondaryHeaderColor,
+          activeColor: Theme.of(context).secondaryHeaderColor,
+          // tabBackgroundColor: ,
+          // tabActiveBorder: ,
+          padding: const EdgeInsets.all(16),
+          gap: 15,
+          tabs: const [
+            GButton(icon: Icons.home, text: 'Home',),
+            GButton(icon: Icons.groups_outlined, text: 'Cooperatives'),
+            GButton(icon: Icons.search, text: 'Search'),
+            GButton(icon: Icons.settings, text: 'Settings')
+          ],
+        ),
+        // drawer: Container(
+        //   width: 250,
+        //   child: Drawer(
+        //       backgroundColor: Theme.of(context).primaryColor,
+        //       child: Column(
+        //         children: <Widget>[
+        //           ListView(shrinkWrap: true, children: [
+        //             DrawerHeader(
+        //                 child: Center(
+        //               child: Text("Turistanginamo",
+        //                   style: Theme.of(context).textTheme.headlineMedium),
+        //             )),
+        //             ListTile(
+        //               leading: const Icon(Icons.home),
+        //               iconColor: Theme.of(context).secondaryHeaderColor,
+        //               title: Text(
+        //                 "Home",
+        //                 style: Theme.of(context).textTheme.headlineMedium,
+        //               ),
+        //               onTap: () {},
+        //             ),
+        //             ListTile(
+        //               leading: const Icon(Icons.groups_outlined),
+        //                                     iconColor: Theme.of(context).secondaryHeaderColor,
+        //               title: Text(
+        //                 "Cooperatives",
+        //                 style: Theme.of(context).textTheme.headlineMedium,
+        //               ),
+        //               onTap: () {},
+        //             ),
+        //           ]),
+        //           Expanded(
+        //               child: Align(
+        //             alignment: FractionalOffset.bottomLeft,
+        //             child: ListTile(
+        //               leading: const Icon(Icons.logout),
+        //                                     iconColor: Theme.of(context).secondaryHeaderColor,
+        //               title: Text(
+        //                 "Logout",
+        //                 style: Theme.of(context).textTheme.headlineMedium,
+        //               ),
+        //               onTap: signOut,
+        //             ),
+        //           ))
+        //         ],
+        //       )),
+        );
+        // ));
   }
 }
