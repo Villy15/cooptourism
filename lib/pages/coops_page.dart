174b8e8b0ed1e2477c649f8bd96a0a1014571faa
Mihoytimoy <timothy_mendoza@dlsu.edu.ph>
2023-09-25 22:10:50 +0800

    progress in cooperatives page

--- lib/pages/coops_page.dart
+++ lib/pages/coops_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class CoopsPage extends StatefulWidget {
   const CoopsPage({super.key});
@@ -8,16 +10,64 @@ class CoopsPage extends StatefulWidget {
 }
 
 class _CoopsPageState extends State<CoopsPage> {
+  final storageRef = FirebaseStorage.instance.ref();
+  final Stream<QuerySnapshot> _cooperatives =
+      FirebaseFirestore.instance.collection('cooperatives').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          "Coops",
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(12.5),
+          child: StreamBuilder<QuerySnapshot>(
+              stream: _cooperatives,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                final cooperatives = snapshot.data!.docs;
+                return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3 / 2,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      mainAxisExtent: 200,
+                    ),
+                    itemCount: cooperatives.length,
+                    itemBuilder: (_, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).secondaryHeaderColor,
+                          borderRadius: BorderRadius.circular(20),
+                          border:
+                              Border.all(width: 1.0, style: BorderStyle.solid),
+                        ),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(16.0),
+                                  topRight: Radius.circular(16.0)),
+                                child: Image.network(
+                                    "${storageRef.child("${cooperatives[index]['logo']}")}",
+                                    height: 170,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                               ),
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              })),
     );
   }
 }
