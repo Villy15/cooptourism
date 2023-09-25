824afae23ad9726bd789c3ddde61a818d3b168e0
Villy15 <adrianvill07@gmail.com>
2023-09-25 19:52:43 +0800

    Add ui for add post and focus on text when clicking

--- lib/pages/add_post.dart
+++ lib/pages/add_post.dart
@@ -1,16 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AddPostPage extends StatelessWidget {
+class AddPostPage extends StatefulWidget {
   const AddPostPage({Key? key}) : super(key: key);
 
   @override
+  AddPostPageState createState() => AddPostPageState();
+}
+
+class AddPostPageState extends State<AddPostPage> {
+  final _contentController = TextEditingController();
+  final _contentFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Request focus when the page is initially loaded
+    _contentFocusNode.requestFocus();
+  }
+
+  void _submitForm() async {
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).colorScheme.primary;
+    // final user = FirebaseAuth.instance.currentUser;
+    // final author = user?.displayName ?? 'Anonymous';
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Post'),
+        backgroundColor: primaryColor,
+        title: const Text(
+          'Add Post',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.post_add),
+            color: Colors.white,
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Add Post Form'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+           Row(
+              children: [
+                Icon(Icons.account_circle, size: 40, color: Theme.of(context).colorScheme.primary),
+                const SizedBox(width: 8),
+                 Text(
+                  'Adrian Villanueva',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _contentController,
+              decoration: const InputDecoration(
+                hintText: "What's on your mind?",
+                border: InputBorder.none,
+              ),
+              maxLines: null,
+              style: TextStyle(fontSize: 18, color: Theme.of(context).colorScheme.primary),
+              // Add color to text background
+              cursorColor: primaryColor,
+              focusNode: _contentFocusNode,
+            ),
+          ],
+        ),
       ),
     );
   }
