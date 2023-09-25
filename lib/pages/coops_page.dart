db7ca31af4a1e8fe1d8439b8bba76303e5a4cde8
Villy15 <adrianvill07@gmail.com>
2023-09-25 22:49:20 +0800

    Add pictures

--- lib/pages/coops_page.dart
+++ lib/pages/coops_page.dart
@@ -19,55 +19,76 @@ class _CoopsPageState extends State<CoopsPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-          padding: const EdgeInsets.all(12.5),
-          child: StreamBuilder<QuerySnapshot>(
-              stream: _cooperatives,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                final cooperatives = snapshot.data!.docs;
-                return GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3 / 2,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
-                      mainAxisExtent: 200,
-                    ),
-                    itemCount: cooperatives.length,
-                    itemBuilder: (_, index) {
-                      return Container(
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).secondaryHeaderColor,
-                          borderRadius: BorderRadius.circular(20),
-                          border:
-                              Border.all(width: 1.0, style: BorderStyle.solid),
+        padding: const EdgeInsets.all(12.5),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _cooperatives,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final cooperatives = snapshot.data!.docs;
+            return GridView.builder(
+              gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    mainAxisExtent: 200,
+                  ),
+              itemCount: cooperatives.length,
+              itemBuilder: (_, index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).secondaryHeaderColor,
+                    borderRadius: BorderRadius.circular(20),
+                    border:
+                        Border.all(width: 1.0, style: BorderStyle.solid),
+                  ),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(16.0),
+                          topRight: Radius.circular(16.0),
                         ),
-                        child: Column(
-                          children: [
-                            ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(16.0),
-                                  topRight: Radius.circular(16.0)),
-                                child: Image.network(
-                                    "${storageRef.child("${cooperatives[index]['logo']}")}",
-                                    height: 170,
-                                    width: double.infinity,
-                                    fit: BoxFit.cover,
-                               ),
-                            ),
-                          ],
+                        child: FutureBuilder<String>(
+                          future: storageRef
+                              .child("iwahori.png")
+                              .getDownloadURL(), // Await here
+                          builder: (context, urlSnapshot) {
+                            if (urlSnapshot.connectionState == ConnectionState.waiting) {
+                              return const CircularProgressIndicator();
+                            }
+
+                            if (urlSnapshot.hasError) {
+                              return Text('Error: ${urlSnapshot.error}');
+                            }
+                            
+                            final imageUrl = urlSnapshot.data;
+
+                            return Image.network(
+                              imageUrl!,
+                              height: 170,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            );
+                          },
                         ),
-                      );
-                    });
-              })),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
