8b4e0ddb7d937dfcaf413a07e743d316116952f4
Anthony Paguio <anthony_paguio@dlsu.edu.ph>
2023-10-09 19:27:45 +0800

    temporary changes to profile section

--- lib/pages/profile/about.dart
+++ lib/pages/profile/about.dart
@@ -13,6 +13,59 @@ class _ProfileAboutState extends State<ProfileAbout> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        userDetails(context, widget.userData),
+        const SizedBox(height: 25),
+        const Divider(
+          color: Color(0xff68707E),
+          thickness: 2,
+        ),
+        const SizedBox(height: 15),
+        userSkills()
+      ],
+    );
+
+   
+  }
+
+  Column userSkills() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Text(
+              'My Skills',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          Container(
+            height: 150,
+            color: Colors.red,
+            child: GridView.count(
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 2,
+              children: [
+                Text('hello')
+              ],
+            )
+          )
+        ],
+      );
+  }
+
+
+  // q: how do i display a certain amount of skills then scroll to see more?
+  // a: use a gridview.count with scrollDirection: Axis.horizontal
+
+
+  Column userDetails(context,Map<String, dynamic> userData ) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
@@ -30,12 +83,6 @@ class _ProfileAboutState extends State<ProfileAbout> {
                 fontSize: 15,
               )),
         ),
-        const SizedBox(height: 25),
-        const Divider(
-          color: Color(0xff68707E),
-          thickness: 2,
-        ),
-
       ],
     );
   }
