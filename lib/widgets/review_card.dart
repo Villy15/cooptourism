455243c9063dbeca41e7a8c0131b7dbc743c19c6
Mihoytimoy <timothy_mendoza@dlsu.edu.ph>
2023-10-12 22:02:35 +0800

    reviews are appearing but the layout is still broken

--- lib/widgets/review_card.dart
+++ lib/widgets/review_card.dart
@@ -1,21 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cooptourism/controller/post_provider.dart';
 import 'package:cooptourism/data/models/review.dart';
-import 'package:cooptourism/data/repositories/cooperative_repository.dart';
-import 'package:cooptourism/widgets/display_profile_picture.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-
 
 class ReviewCard extends StatefulWidget {
   final ReviewModel? reviewModel;
 
-  const ReviewCard({super.key, 
-    this.reviewModel
-  });
+  const ReviewCard({super.key, this.reviewModel});
 
   @override
   State<ReviewCard> createState() => _ReviewCardState();
@@ -39,11 +30,11 @@ class _ReviewCardState extends State<ReviewCard> {
 
   @override
   Widget build(BuildContext context) {
-    final storageRef = FirebaseStorage.instance.ref();
+    // final storageRef = FirebaseStorage.instance.ref();
 
-    final cooperativeRepository = CooperativesRepository();
-    final cooperative =
-        cooperativeRepository.getCooperative(widget.reviewModel?.id ?? "");
+    // final cooperativeRepository = CooperativesRepository();
+    // final cooperative =
+    //     cooperativeRepository.getCooperative(widget.reviewModel?.id ?? "");
     return const Text("This works for sure");
   }
 }
