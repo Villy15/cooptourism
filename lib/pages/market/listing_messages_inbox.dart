7456ee22cf29293ac996d8832c4613963ce44704
Villy15 <adrianvill07@gmail.com>
2023-11-06 17:28:58 +0800

    Add Services in member home, Add notifications w/o backcend

--- lib/pages/market/listing_messages_inbox.dart
+++ lib/pages/market/listing_messages_inbox.dart
@@ -44,7 +44,7 @@ class _ListingMessagesInboxState extends ConsumerState<ListingMessagesInbox> {
           debugPrint("testing the print ${receivedFrom.length}");
           if (receivedFrom.isNotEmpty) {
             return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
               child: ListView.builder(
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
