55091616d85cc306d88d2048541c537671c984b3
jaz8c <justice_campanilla@dlsu.edu.ph>
2023-11-11 22:53:19 +0800

    Started loans page

--- lib/pages/wallet/wallet_page.dart
+++ lib/pages/wallet/wallet_page.dart
@@ -12,12 +12,32 @@ class _WalletPageState extends State<WalletPage> {
   final List<String> _tabTitles = ['Wallets', 'Loans', 'Donate'];
   int _selectedIndex = 0;
 
-  @override
+   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(context, "Wallet"),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            listFilter(),
+            const SizedBox(height: 10),
+            _selectedIndex == 0
+                ? _buildWalletsContent()
+                : _selectedIndex == 1
+                    ? _buildLoansContent()
+                    : _buildDonateContent(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWalletsContent() {
     String name = "Adrian";
     double money = 1000.00;
 
-    // Create a sample of a transactions where it has product, timestamp, amount
+    // Create a sample of transactions where it has product, timestamp, amount
     List<Transaction> transactions = [
       Transaction(product: 'Item 1', timestamp: DateTime.now(), amount: 10.0),
       Transaction(product: 'Item 2', timestamp: DateTime.now(), amount: 20.0),
@@ -30,34 +50,42 @@ class _WalletPageState extends State<WalletPage> {
       Transaction(product: 'Item 3', timestamp: DateTime.now(), amount: 30.0),
     ];
 
-    return Scaffold(
-      appBar: _appBar(context, "Wallet"),
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            listFilter(),
-            const SizedBox(height: 10),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-              child: walletWidget(context, name, money),
-            ),
-            const SizedBox(height: 10),
-    
-            // ListsView Builder of transactions table
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-              child: transactionHeading(),
-            ),
-            listTransactions(transactions)
-          ],
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+            child: walletWidget(context, name, money),
+          ),
+          const SizedBox(height: 10),
+
+          // ListsView Builder of transactions table
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: transactionHeading(),
+          ),
+          listTransactions(transactions),
+        ],
       ),
     );
   }
 
+  Widget _buildLoansContent() {
+    double maxLoan = 50000.00;
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+      child: loansWidget(context, maxLoan),
+    );
+  }
+
+  Widget _buildDonateContent() {
+    // Content for the 'Donate' tab
+    return Container(
+      // Replace this with your 'Donate' tab content
+      child: Text('Donate Content'),
+    );
+  }
+  
   Row transactionHeading() {
     return const Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -204,6 +232,91 @@ class _WalletPageState extends State<WalletPage> {
       ),
     );
   }
+
+Container loansWidget(BuildContext context, double maxLoan) {
+  return Container(
+    height: 300, // Adjust the height as needed
+    width: 600,
+    decoration: BoxDecoration(
+      color: Theme.of(context).colorScheme.secondary,
+      borderRadius: BorderRadius.circular(20.0),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Max Loan Amount"),
+          const SizedBox(height: 4),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("₱${maxLoan.toStringAsFixed(2)}",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 40)),
+            ],
+          ),
+
+          // Additional Content Section
+          const Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Text 1
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Payable in: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15)),
+                  VerticalDivider(
+                    width: 10,
+                    thickness: 2, // Adjust the thickness as needed
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+
+              // Text 2
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Interest Rate",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15)),
+                  VerticalDivider(
+                    width: 80,
+                    thickness: 2, // Adjust the thickness as needed
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+
+              // Text 3
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Fees",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15)),
+                  VerticalDivider(
+                    width: 10,
+                    thickness: 8, // Adjust the thickness as needed
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+
+
+
 
   SizedBox listFilter() {
     return SizedBox(
