341ceca0dd570cda390d79f9d20fe7e2b70b4e70
Villy15 <adrianvill07@gmail.com>
2023-11-17 00:26:49 +0800

    Add loans in member

---
+++ lib/pages/wallet/loan_application.dart
@@ -0,0 +1,228 @@
+import 'package:cooptourism/core/theme/light_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LoanApplicationPage extends ConsumerStatefulWidget {
+  const LoanApplicationPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _LoanApplicationPageState();
+}
+
+class _LoanApplicationPageState extends ConsumerState<LoanApplicationPage> {
+  double _loanAmount = 50000.0;
+  String _selectedDuration = '6 Months';
+  String _selectedReason = 'Replenish Supply';
+  final TextEditingController _detailedReasonController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(context, 'Loans'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              loanSlider(),
+
+              durationDropDown(),
+              const SizedBox(height: 16.0),
+
+              loanSummaryReason(),
+              const SizedBox(height: 16.0),
+
+              loanDetailedReason(),
+              const SizedBox(height: 16.0),
+
+              loanDetails(),
+
+              // ... Add more fields for Duration, Loan Reason, Detailed Reason, etc. ...
+              continueBtn(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding continueBtn() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {
+            // Trigger loan application submission
+          },
+          child: const Text('Continue'),
+        ),
+      ),
+    );
+  }
+
+  Padding loanDetails() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: const Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Interest per Month'),
+                Text('0%'),
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Fees'),
+                Text('₱100.00'),
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Duration'),
+                Text('12 Months'),
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Amount Due'),
+                Text('₱5,000.00'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextFormField loanDetailedReason() {
+    return TextFormField(
+      controller: _detailedReasonController,
+      decoration: const InputDecoration(
+        labelText: 'Detailed Reason (optional)',
+        contentPadding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 20.0),
+      ),
+      maxLines: 3,
+    );
+  }
+
+  DropdownButtonFormField<String> loanSummaryReason() {
+    return DropdownButtonFormField<String>(
+      value: _selectedReason,
+      decoration: const InputDecoration(
+        labelText: 'Loan Reason',
+        contentPadding: EdgeInsets.symmetric(horizontal: 12.0),
+      ),
+      items: <String>[
+        'Replenish Supply',
+        'Expand Business',
+        'Personal Use',
+        'Other'
+      ].map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedReason = newValue!;
+        });
+      },
+    );
+  }
+
+  DropdownButtonFormField<String> durationDropDown() {
+    return DropdownButtonFormField<String>(
+      value: _selectedDuration,
+      decoration: const InputDecoration(
+        labelText: 'Duration',
+        contentPadding: EdgeInsets.symmetric(horizontal: 12.0),
+      ),
+      items: <String>['6 Months', '12 Months', '18 Months', '24 Months']
+          .map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedDuration = newValue!;
+        });
+      },
+    );
+  }
+
+  Column loanSlider() {
+    return Column(
+      children: [
+        ListTile(
+          title: const Text("Loan Amount"),
+          subtitle: Text("₱ ${_loanAmount.toStringAsFixed(2)}"),
+          trailing: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text("₱ 1,000 min"),
+              Text("₱ 250,000 max"),
+            ],
+          ),
+        ),
+        Slider(
+          value: _loanAmount,
+          min: 1000,
+          max: 250000,
+          divisions: 249,
+          label: _loanAmount.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _loanAmount = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  AppBar _appBar(BuildContext context, String title) {
+    return AppBar(
+      // Assuming primaryColor is defined in your dark_theme.dart
+      iconTheme: const IconThemeData(color: primaryColor),
+      toolbarHeight: 70,
+      title: Text(title,
+          style: TextStyle(
+              fontSize: 28, color: Theme.of(context).colorScheme.primary)),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0),
+          child: CircleAvatar(
+            backgroundColor: Colors.grey.shade300,
+            child: IconButton(
+              onPressed: () {
+                // Settings or additional action
+              },
+              icon: const Icon(Icons.settings, color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/wallet/wallet_page.dart
+++ lib/pages/wallet/wallet_page.dart
@@ -1,6 +1,7 @@
 import 'package:cooptourism/data/models/manager_dashboard.dart/sales.dart';
 import 'package:cooptourism/data/repositories/listing_repository.dart';
 import 'package:cooptourism/data/repositories/manager_dashboard/sales_repository.dart';
+import 'package:cooptourism/pages/wallet/loan_application.dart';
 import 'package:cooptourism/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,7 +21,7 @@ class _WalletPageState extends ConsumerState<WalletPage> {
   final List<String> _tabTitles = ['Transactions', 'Loans', 'Donate'];
   int _selectedIndex = 0;
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(context, "Finance"),
@@ -44,49 +45,138 @@ class _WalletPageState extends ConsumerState<WalletPage> {
   Widget _buildWalletsContent() {
     final user = ref.watch(userModelProvider);
     return StreamBuilder<Object>(
-      stream: user?.role! == 'Customer' ? salesRepository.getAllSalesByCustomerId(user!.uid!) : salesRepository.getAllSalesByOwnerId(user!.uid!),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(child: CircularProgressIndicator());
-        } 
+        stream: user?.role! == 'Customer'
+            ? salesRepository.getAllSalesByCustomerId(user!.uid!)
+            : salesRepository.getAllSalesByOwnerId(user!.uid!),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-        final transactions = snapshot.data as List<SalesData>;
-        // Sort transactions by date
-        transactions.sort((a, b) => b.date.compareTo(a.date));
+          final transactions = snapshot.data as List<SalesData>;
+          // Sort transactions by date
+          transactions.sort((a, b) => b.date.compareTo(a.date));
 
-        return Column(
-          children: [
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-            //   child: walletWidget(context, name, money),
-            // ),
-            // const SizedBox(height: 10),
-        
-            // ListsView Builder of transactions table
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-              child: transactionHeading(),
-            ),
-            listTransactions(transactions),
-          ],
-        );
-      }
-    );
+          return Column(
+            children: [
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+              //   child: walletWidget(context, name, money),
+              // ),
+              // const SizedBox(height: 10),
+
+              // ListsView Builder of transactions table
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                child: transactionHeading(),
+              ),
+              listTransactions(transactions),
+            ],
+          );
+        });
   }
 
   Widget _buildLoansContent() {
-    double maxLoan = 50000.00;
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-      child: loansWidget(context, maxLoan),
+    double maxLoan = 250000.00;
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+          child: loansWidget(context, maxLoan),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+          child: _buildScrollableTerms(),
+        ),
+        const SizedBox(height: 10),
+        // Add a Start button that expands to the full width
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoanApplicationPage(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+              ),
+              child: const Text("Start Application",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 16)),
+            ),
+          ),
+        ),
+      ],
     );
   }
+
+  Widget _buildScrollableTerms() {
+  return Container(
+    padding: const EdgeInsets.all(16.0),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12.0),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 4,
+          offset: Offset(0, 2),
+        ),
+      ],
+    ),
+    // Use a ConstrainedBox to limit the height of the SingleChildScrollView
+    child: ConstrainedBox(
+      constraints: const BoxConstraints(
+        // Set a max height for the scrollable area
+        maxHeight: 320.0,
+      ),
+      child: const Scrollbar (
+        thumbVisibility: true,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Loan terms and conditions\n',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'The terms and conditions upon which each loan shall be granted and repaid shall include but not be limited to the following—\n\n'
+                'a. every application for a loan shall be accompanied by such information about the financial position and income of the borrower as the Credit Committee or loans officer may require;\n\n'
+                'b. no society shall make a loan to an unincorporated organization. Where such a loan is contemplated, it shall be made to one or more of the members or officers of the organisation provided, however; that the society shall, in any such particular case, require such additional security by way of endorsement of the promissory note as may be deemed desirable;\n\n'
+                'c. no loan shall be made to a member if it would cause the total indebtedness of the member to the credit union to exceed 10 percent of the paid-up capital and deposits of the credit union;\n\n'
+                'd. the total of all loans made to associations, organizations or corporations, shall not, at any time, exceed 25 percent of the total shares and deposits of the credit union;\n\n'
+                'e. no loan shall be made to a company unless such loan is personally guaranteed by shareholders of the company holding a majority of the shares in value and in voting rights provided that such personal guarantee shall not be required where the loan is guaranteed by an organization or agency of Government;\n\n'
+                'f. no loan shall be made by a society to a corporation if a majority of the shares of the corporation are held by the officers and directors of the credit union unless the application has been approved by the Registrar;\n\n'
+                'g. transactions in the loan account of a member shall be shown by the necessary entries in a passbook or statement to be delivered to each member;\n\n'
+                'h. where a mortgage on land or building is taken as security for a loan, the amount loaned shall not exceed 90 percent of the market value of the land or buildings;\n\n'
+                'i. before such a loan is made, the Credit Committee or loan officer shall require that an appraisal of the market value of the property be made by an appraiser whom they believe to be competent and who is instructed and employed by the Credit Union independently of any owner of the property on a form approved by the Registrar;\n\n'
+                'j. the expenses, if any, of any appraiser employed pursuant to subparagraph (i) may be borne by the applicant for the loan.\n',
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
 
   Widget _buildDonateContent() {
     // Content for the 'Donate' tab
     return const Text('Donate Content');
   }
-  
+
   Row transactionHeading() {
     return const Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -114,28 +204,28 @@ class _WalletPageState extends ConsumerState<WalletPage> {
             DateFormat('MMM dd yyyy | hh:mm a').format(sale.date);
         return ListTile(
           leading: const Icon(Icons.payment),
-          title: FutureBuilder (
-            future: listingRepository.getListingTitle(sale.listingId!),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
-              } 
-              final listingTitle = snapshot.data as String;
-              return Text(listingTitle,
-                style: TextStyle(
-                    fontSize: 17,
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold));
-            }
-          ),
+          title: FutureBuilder(
+              future: listingRepository.getListingTitle(sale.listingId!),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                final listingTitle = snapshot.data as String;
+                return Text(listingTitle,
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold));
+              }),
           subtitle: Text(formattedDate,
               style: TextStyle(
                   fontSize: 14,
                   color: Theme.of(context).colorScheme.primary,
                   fontWeight: FontWeight.w400)),
           trailing: Text(
-              user?.role == 'Member' ||  user?.role ==  'Manager' ? "${sale.sales >= 0 ? '+' : '-'}₱${sale.sales.abs().toStringAsFixed(2)} " 
-                : "${sale.sales <= 0 ? '+' : '-'}₱${sale.sales.abs().toStringAsFixed(2)} ",
+              user?.role == 'Member' || user?.role == 'Manager'
+                  ? "${sale.sales >= 0 ? '+' : '-'}₱${sale.sales.abs().toStringAsFixed(2)} "
+                  : "${sale.sales <= 0 ? '+' : '-'}₱${sale.sales.abs().toStringAsFixed(2)} ",
               style: TextStyle(
                   fontSize: 18,
                   color: Theme.of(context).colorScheme.primary,
@@ -245,90 +335,47 @@ class _WalletPageState extends ConsumerState<WalletPage> {
     );
   }
 
-Container loansWidget(BuildContext context, double maxLoan) {
-  return Container(
-    height: 300, // Adjust the height as needed
-    width: 600,
-    decoration: BoxDecoration(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(20.0),
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("Max Loan Amount"),
-          const SizedBox(height: 4),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("₱${maxLoan.toStringAsFixed(2)}",
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 40)),
-            ],
-          ),
-
-          // Additional Content Section
-          const Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Text 1
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Payable in: ",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15)),
-                  VerticalDivider(
-                    width: 10,
-                    thickness: 2, // Adjust the thickness as needed
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-
-              // Text 2
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Interest Rate",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15)),
-                  VerticalDivider(
-                    width: 80,
-                    thickness: 2, // Adjust the thickness as needed
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-
-              // Text 3
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Fees",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15)),
-                  VerticalDivider(
-                    width: 10,
-                    thickness: 8, // Adjust the thickness as needed
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-            ],
+  Container loansWidget(BuildContext context, double maxLoan) {
+    return Container(
+      height: 150, // Adjust the height as needed
+      width: double.infinity, // This will take the full width available
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 8.0,
+            offset: Offset(0, 2),
           ),
         ],
       ),
-    ),
-  );
-}
-
-
-
-
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Max Loan Amount",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+            const SizedBox(height: 8),
+            Text("₱${NumberFormat('#,##0.00', 'en_US').format(maxLoan)}",
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+            const SizedBox(height: 24), // Space between amount and terms
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Payable in\n6-12 months", style: TextStyle(fontSize: 14)),
+                Text("Interest Rate\n0% per month",
+                    style: TextStyle(fontSize: 14)),
+                Text("Fees\n2% one-time", style: TextStyle(fontSize: 14)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   SizedBox listFilter() {
     return SizedBox(
@@ -372,12 +419,12 @@ Container loansWidget(BuildContext context, double maxLoan) {
     );
   }
 
-  
-
   AppBar _appBar(BuildContext context, String title) {
     return AppBar(
       toolbarHeight: 70,
-      title: Text(title, style: TextStyle(fontSize: 28, color: Theme.of(context).colorScheme.primary)),
+      title: Text(title,
+          style: TextStyle(
+              fontSize: 28, color: Theme.of(context).colorScheme.primary)),
       iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
       // actions: [
       //   Padding(
@@ -396,4 +443,3 @@ Container loansWidget(BuildContext context, double maxLoan) {
     );
   }
 }
-
